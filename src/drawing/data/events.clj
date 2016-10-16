b1bb56bcf2aa441b01b8845c3680da176eecf78c
Levshits Valentin <levshits@yahoo.com>
2016-10-17 01:36:22 +0300

    Getting events after selected was implemented

--- src/drawing/data/events.clj
+++ src/drawing/data/events.clj
@@ -2,7 +2,8 @@
   (:use [drawing.data.provider :as provider]
         [drawing.configuration :as config]
         [monger.collection :as mc]
-        [monger.conversion :refer [from-db-object]]))
+        [monger.conversion :refer [from-db-object]]
+        [monger.operators :refer :all]))
 
 (defn insert-event [room-id event]
   "Function that insert new event and return inserted object"
@@ -10,7 +11,13 @@
                             (mc/insert-and-return db room-id event))))
 
 (defn get-all-events [room-id]
-  "Function retunrns all events from collection"
+  "Function returns all events from collection"
   (provider/execute-query (fn [db]
                             (sort-by :id
                                      (mc/find-maps db room-id)))))
+
+(defn get-events-after [room-id event-id]
+  "Function returns all events that occured after event with id event-id"
+  (provider/execute-query (fn [db]
+                            (sort-by :id
+                                     (mc/find-maps db room-id {:id {"$gt" event-id}})))))
