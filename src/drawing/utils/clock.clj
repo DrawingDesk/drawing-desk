2f33bf5631957a9c2f42fe378b2291ddb032c612
Levshits Valentin <levshits@yahoo.com>
2016-11-07 17:07:47 +0300

    Clock mechanism is implemented

--- src/drawing/utils/clock.clj
+++ src/drawing/utils/clock.clj
@@ -2,7 +2,20 @@
 
 (def room-counters (agent {}))
 
+(defn- -create-new-room-clock [clocks room-id]
+  (println "Create new")
+  (if (not (contains? clocks room-id))
+    (assoc clocks room-id (atom 0))))
+
+(defn- -get-or-create-room-clock [room-id]
+  "Function return or create new vector clock for room"
+  (if (not (contains? @room-counters room-id))
+    (send room-counters -create-new-room-clock room-id))
+  (await room-counters)
+  (get @room-counters room-id))
+
+
 (defn next-value [room-id]
   "Function returns next Lamport clock value"
   (println "Mocked func")
-  (+ 0 1))
+  (swap! (-get-or-create-room-clock room-id) inc))
