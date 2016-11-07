837a8a3cbd93bf5fc12578ce01747c8913e619f1
Levshits Valentin <levshits@yahoo.com>
2016-11-07 18:45:36 +0300

    Some bugs fixed

--- src/drawing/data/events_dao.clj
+++ src/drawing/data/events_dao.clj
@@ -1,6 +1,7 @@
 (ns drawing.data.events-dao
   (:refer-clojure :exclude [find sort])
-  (:use [drawing.data.provider :as provider]
+  (:use [clojure.core :only [read-string]]
+        [drawing.data.provider :as provider]
         [monger.collection :only [find-maps insert-and-return]]
         [monger.conversion :refer [from-db-object]]
         [monger.operators :refer :all]
@@ -20,14 +21,14 @@
 (defn get-events-after [room-id sync-id]
   "Function returns all events that occured after event with id sync-id"
   (provider/execute-query (fn [db]
+                            (println room-id sync-id)
                             (sort-by :sync-id
-                                     (find-maps db room-id {:sync-id {"$gt" sync-id}})))))
-
+                                     (find-maps db room-id {:sync-id {"$gt" (read-string sync-id)}})))))
 (defn get-last-event [room-id]
   (let [events (provider/execute-query (fn [db]
-                                             (with-collection db room-id
-                                                              (find {})
-                                                              (fields [:sync-id :_id])
-                                                              (sort (array-map :sync-id -1))
-                                                              (limit 1))))]
+                                         (with-collection db room-id
+                                                          (find {})
+                                                          (fields [:sync-id :_id])
+                                                          (sort (array-map :sync-id -1))
+                                                          (limit 1))))]
     (first events)))
